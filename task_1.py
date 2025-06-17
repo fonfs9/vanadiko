8cab2157cdd64a74593674dbaf07061bc4e000bd
vanadiko <anisimov.ivan2010@gmail.com>
2025-06-17 15:36:24 +0400

    сделал простой калькулятор, считающий сумму,разность,произведение и частное. добавил высчет площади прямоугольника, использовал float для высчета целых и дробных чисел

--- task_1.py
+++ task_1.py
@@ -1,3 +1,15 @@
 def func():
-    return 1 + 2
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
 
+    print("Сумма:", a + b)
+    print("Разность:", a - b)
+    print("Произведение:", a * b)
+    print("Частное:", a / b)
+
+    length = float(input("Введите длину прямоугольника: "))
+    width = float(input("Введите ширину прямоугольника: "))
+    area = length * width
+    print('площадь прямоугольника',area)
+
+func()
