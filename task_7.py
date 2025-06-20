5c8f7c48ee519a5ea7b389feea4c2b1089302f70
vanadiko <anisimov.ivan2010@gmail.com>
2025-06-21 00:00:48 +0400

    создал множество чисел, улучшил задачу,  добавив ввод от пользователя , создал два мнодества и вывел разность пересечение и обьединение

---
+++ task_7.py
@@ -0,0 +1,45 @@
+""""Создай множество из списка с повторяющимися значениями.
+
+Проверь, содержится ли элемент в множестве.
+
+Создай два множества и выведи:
+
+их объединение
+
+пересечение
+
+разность
+"""
+
+def dasd():
+    numbers_list = [1, 2, 2, 3, 4, 4, 5]
+
+    numbers_set = set(numbers_list)
+
+    print("Множество:", numbers_set)
+
+dasd()
+
+def osamason():
+
+    numbers = ['1', '2', '2', '3', '4', '4', '5']
+    key = input('введите число')
+    if key in numbers:
+        print("Число есть в списке!")
+    else:
+        print("Числа нет в списке.")
+osamason()
+
+def ccc():
+    set1 = {1, 2, 3, 4}
+    set2 = {3, 4, 5, 6}
+
+    union = set1 | set2
+    print("Объединение:", union)
+
+    intersection = set1 and set2
+    print("Пересечение:", intersection)
+
+    difference = set1 - set2
+    print("Разность:", difference)
+ccc()
